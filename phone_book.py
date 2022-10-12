16b970be05746ae15a7207ef2ea72ad03d04c83a
cassien babey <cassien98@yahoo.fr>
2022-10-12 11:13:58 +0200

    Read.csv doc

--- phone_book.py
+++ phone_book.py
@@ -1,1 +1,6 @@
+import csv
 
+with open('data/phone_book.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        print(row['last_name'],":",int(row['phone_number']))
